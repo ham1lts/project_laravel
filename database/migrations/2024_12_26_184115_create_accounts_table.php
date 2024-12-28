b313f27d4358231332d6c2c5376ce8579447b884
ham1lts <hamiltonfreire.a@gmail.com>
2024-12-28 10:11:04 -0300

    chore: update columns balance and account_number in account table

--- database/migrations/2024_12_26_184115_create_accounts_table.php
+++ database/migrations/2024_12_26_184115_create_accounts_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
-            $table->mediumInteger('account_number')->unique();
-            $table->unsignedFloat('balance')->default(0);
+            $table->integer('account_number')->unique();
+            $table->unsignedFloat('balance', 10)->default(0);
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'))->useCurrentOnUpdate();
         });
